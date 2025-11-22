9fae3925364968768c76578fe3d36ce95091bce3
JatinGautam13 <166696883+JatinGautam13@users.noreply.github.com>
2025-11-22 09:54:24 +0000

    sh

--- src/app/contact-us/page.js
+++ src/app/contact-us/page.js
@@ -1,18 +1,14 @@
-import AboutSection1 from "@/components/AboutSection/AboutSection1";
-import AboutSection2 from "@/components/AboutSection/AboutSection2";
-import HomeSection7 from "@/components/HomeSection/HomeSection7";
-import "@/styles/blog.css";
+import ContactSection from "@/components/ContactSection/ContactSection";
+// import "@/styles/blog.css";
 
 export const metadata = {
-  title: "About",
+  title: "Contact",
 };
 
-export default function AboutPage() {
+export default function ContactPage() {
   return (
     <main >
-      <AboutSection1 />
-      <AboutSection2 />
-      <HomeSection7 />
+      <ContactSection />
     </main>
   );
 }
