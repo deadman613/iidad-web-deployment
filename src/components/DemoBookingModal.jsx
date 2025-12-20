d9fdca7e11d460299240a3864f3565b1962f3d30
Manaregr8 <manjeetdevelops@gmail.com>
2025-12-20 23:56:32 +0530

    whkhq

--- src/components/DemoBookingModal.jsx
+++ src/components/DemoBookingModal.jsx
@@ -60,7 +60,7 @@ export default function DemoBookingModal({ open: propOpen, onClose: propOnClose 
     };
 
     try {
-      const response = await fetch('https://script.google.com/macros/s/AKfycbzX2pbdedAJbFzL0EF2DvfY0x40TTEEYjVXiubmj03AxxOSlWejnNQCBhOiqNklLCeo/exec', {
+      const response = await fetch('https://script.google.com/macros/s/AKfycbxH1wyzn3yUQLDKmwWIETp4oL5J6o2TBTGtb2dnCn54CHEc_zDoULNgOQv0p7MnsNPR/exec', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(payload)
